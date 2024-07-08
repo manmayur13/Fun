3b312b4d25baa2832f23378fe0852ea17d8d651e
manmayur13 <patilmayur16777@gmail.com>
2024-07-08 22:23:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// document.querySelector("h1").innerHTML="mayur";
+
+
+function checkevenodd(num){
+    let result=num%2;
+    if(result){
+        alert("The number is odd");
+    }
+    else{
+        alert("The number is even");
+        
+    }
+    
+}
+
+function getsum(a,b){
+    let result=a+b;
+    alert(result);
+    
+}
+
+
+
